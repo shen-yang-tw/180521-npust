2b8875f688195ef4cb9ed2b43707e52fef836343
shen-yang-tw <shen.yang.work.tw@gmail.com.tw>
2018-06-11 15:31:54 +0800

    0611-修改

--- js/script.js
+++ js/script.js
@@ -14,15 +14,39 @@ $(document).ready(function() {
   //Remove if empty
   $("p:empty, h1:empty, h2:empty, h3:empty, h4:empty, h5:empty, h6:empty").remove();
 
-  //font resize
-  $('.large>a').click(function() {
-    $('.font_resize').removeClass('font_medium').addClass('font_large');
+  //for Plus Search page
+  $("#search_option>div:nth-child(2)").after($("#search_option>div:first").clone().removeClass('uk-row-first'));
+  $("#search_option [data-uk-alert]:nth-last-child(3) .option").remove();
+  $("#search_option>div:first button").removeClass('uk-alert-close');
+  $("#add").click(function() {
+    //button #add cannot place in <form> or won't working
+    var counter = $("#search_option>div").length;
+    $("#search_option>div:first button").addClass('uk-alert-close');
+    var newSearch = $("#search_option>div:first").clone().removeClass('uk-row-first');
+    if (counter < 8) {
+      if (counter < 5) {
+        $("#search_option>div:first button").removeClass('uk-alert-close');
+      }
+      $("#search_option>div:nth-child(2)").after(newSearch);
+      $("#search_option [data-uk-alert]:nth-last-child(3) .option").remove();
+    }
+    $("#search_option>div:first button").removeClass('uk-alert-close');
   });
-  $('.medium>a').click(function() {
-    $('.font_resize').removeClass('font_large').addClass('font_medium');
+
+  //Uikit 2 - accordion toggleClass class when open/close
+  $('.uk-accordion').on('toggle.uk.accordion', function() {
+    $(this).children(".accordion_title").toggleClass('open');
   });
-  $('.small>a').click(function() {
-    $('.font_resize').removeClass('font_large').removeClass('font_medium');
+
+  //remove self after show .listMore
+  $(".btnMore").click(function() {
+    $(this).parent().siblings(".listMore").removeClass("uk-hidden");
+    $(this).parent().remove();
+  });
+
+  //uikit 2 - searchBar sticky under #header
+  $("#searchBar").on('active.uk.sticky', function() {
+    $(this).css("top", $("#header").height());
   });
 
 });
